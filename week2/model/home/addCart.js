641c51f0b41bcbfc9b510317ca355c3bb3fd9314
wuzoo <wndyd9911@gmail.com>
2024-04-07 21:18:25 +0900

    feat: 가격에서 "원" 제거 + "," 추가한 convertedValue를 저장

--- week2/model/home/addCart.js
+++ week2/model/home/addCart.js
@@ -1,19 +1,37 @@
 const item = document.querySelectorAll(".card");
 
 function handleAddCart(e) {
-  const ok = confirm("장바구니에 추가하시겠습니까 ?");
+  const ok = confirm("장바구니에 추가하시겠습니까 ?"); // confirm 텍스트 보여주기
 
   if (!ok) return;
 
-  const [img, like, title, value] = e.currentTarget.children;
+  const [img, like, title, value] = e.currentTarget.children; // card 요소의 자식 요소 가져오기
   const list = JSON.parse(localStorage.getItem("list")) ?? [];
 
   const count = list.length;
 
+  let convertedValue = "";
+
+  const tmp = value.innerText.replace("원", ""); // "원" 제거
+
+  tmp
+    .split("")
+    .reverse()
+    .forEach((char, idx) => {
+      // 가격 배열을 거꾸로 순회하며 3번째마다 "," 추가
+      if (idx % 3 === 0 && idx !== 0) {
+        convertedValue += "," + char;
+        return;
+      }
+      convertedValue += char;
+    });
+
+  convertedValue = convertedValue.split("").reverse().join(""); // 다시 순서 바꾸고 join
+
   list.push({
     id: count + 1,
     title: title.innerText,
-    value: value.innerText,
+    value: convertedValue,
   });
 
   localStorage.setItem("list", JSON.stringify(list));
