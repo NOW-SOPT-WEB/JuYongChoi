b49d2da13c3d77d1cf81866a9a44a39a5f6a695d
wuzoo <wndyd9911@gmail.com>
2024-04-08 13:49:45 +0900

    chore: basic에 포함되지 않는 기능 -> 주석 처리

--- week2/model/cart/check.js
+++ week2/model/cart/check.js
@@ -1,42 +1,42 @@
-const checkAllBtn = document.getElementById("check-all-btn");
-const checkboxs = document.querySelectorAll(".cart_table tr");
+// const checkAllBtn = document.getElementById("check-all-btn");
+// const checkboxs = document.querySelectorAll(".cart_table tr");
 
-function handleAllChecked(e) {
-  const checked = e.target.checked;
-  const allCheckBox = document.querySelectorAll(".check_item") ?? [];
-  const storeList = JSON.parse(localStorage.getItem("list"));
+// function handleAllChecked(e) {
+//   const checked = e.target.checked;
+//   const allCheckBox = document.querySelectorAll(".check_item") ?? [];
+//   const storeList = JSON.parse(localStorage.getItem("list"));
 
-  allCheckBox.forEach((input) => {
-    input.checked = checked;
-  });
+//   allCheckBox.forEach((input) => {
+//     input.checked = checked;
+//   });
 
-  localStorage.setItem("checkList", JSON.stringify(checked ? storeList : []));
-}
+//   localStorage.setItem("checkList", JSON.stringify(checked ? storeList : []));
+// }
 
-function handleChecked(e, itemId) {
-  const checked = e.target.checked;
+// function handleChecked(e, itemId) {
+//   const checked = e.target.checked;
 
-  const storeList = JSON.parse(localStorage.getItem("list"));
-  const storeCheckList = JSON.parse(localStorage.getItem("checkList")) ?? [];
+//   const storeList = JSON.parse(localStorage.getItem("list"));
+//   const storeCheckList = JSON.parse(localStorage.getItem("checkList")) ?? [];
 
-  const checkedItem = storeList.find((item) => item.id === +itemId);
+//   const checkedItem = storeList.find((item) => item.id === +itemId);
 
-  if (checked) {
-    storeCheckList.push(checkedItem);
-  } else {
-    const idx = storeCheckList.findIndex((item) => item.id === +itemId);
-    storeCheckList.splice(idx, 1);
-  }
-  localStorage.setItem("checkList", JSON.stringify(storeCheckList));
-}
+//   if (checked) {
+//     storeCheckList.push(checkedItem);
+//   } else {
+//     const idx = storeCheckList.findIndex((item) => item.id === +itemId);
+//     storeCheckList.splice(idx, 1);
+//   }
+//   localStorage.setItem("checkList", JSON.stringify(storeCheckList));
+// }
 
-checkAllBtn.addEventListener("change", (e) => handleAllChecked(e));
+// checkAllBtn.addEventListener("change", (e) => handleAllChecked(e));
 
-checkboxs.forEach((checkbox, idx) => {
-  if (idx === 0) return;
+// checkboxs.forEach((checkbox, idx) => {
+//   if (idx === 0) return;
 
-  const box = checkbox.children[0].querySelector("input");
-  const id = checkbox.getAttribute("id");
+//   const box = checkbox.children[0].querySelector("input");
+//   const id = checkbox.getAttribute("id");
 
-  box.addEventListener("change", (e) => handleChecked(e, id));
-});
+//   box.addEventListener("change", (e) => handleChecked(e, id));
+// });
