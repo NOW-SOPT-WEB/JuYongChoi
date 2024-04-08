3eaba83d9d8d4db095113c1b201c034b0fd6e865
wuzoo <wndyd9911@gmail.com>
2024-04-08 13:55:56 +0900

    chore: basic에 포함되지 않는 기능 -> 주석 처리

--- week2/model/cart/deleteItem.js
+++ week2/model/cart/deleteItem.js
@@ -1,15 +1,15 @@
-const tableItemBtn = document.querySelectorAll(".cart_table button");
-const list = JSON.parse(localStorage.getItem("list"));
+// const tableItemBtn = document.querySelectorAll(".cart_table button");
+// const list = JSON.parse(localStorage.getItem("list"));
 
-function handleDelete(id) {
-  const deleted = list.filter((item) => item.id !== +id);
-  localStorage.setItem("list", JSON.stringify(deleted));
+// function handleDelete(id) {
+//   const deleted = list.filter((item) => item.id !== +id);
+//   localStorage.setItem("list", JSON.stringify(deleted));
 
-  window.location.reload();
-}
+//   window.location.reload();
+// }
 
-tableItemBtn.forEach((btn) => {
-  const id = +btn.className.match(/\d+/);
+// tableItemBtn.forEach((btn) => {
+//   const id = +btn.className.match(/\d+/);
 
-  btn.addEventListener("click", () => handleDelete(id));
-});
+//   btn.addEventListener("click", () => handleDelete(id));
+// });
