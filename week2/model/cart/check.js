5f442d204672df2b58e6beda33312b6a40bc0588
wuzoo <wndyd9911@gmail.com>
2024-04-08 11:16:38 +0900

    feat: 전체 checkbox, 개별 checkbox 클릭 시 checkList 저장

---
+++ week2/model/cart/check.js
@@ -0,0 +1,44 @@
+const checkAllBtn = document.getElementById("check-all-btn");
+const checkboxs = document.querySelectorAll(".cart_table tr");
+
+const storeList = JSON.parse(localStorage.getItem("list"));
+const newCheckkList = [];
+
+function handleAllChecked(e) {
+  const checked = e.target.checked;
+  const allCheckBox = document.querySelectorAll(".check_item");
+
+  allCheckBox.forEach((input) => {
+    input.checked = checked;
+  });
+
+  localStorage.setItem("checkList", JSON.stringify(checked ? storeList : []));
+}
+
+function handleChecked(e, itemId) {
+  const checked = e.target.checked;
+
+  const checkedItem = storeList.find((item) => item.id === +itemId);
+
+  if (checked) {
+    newCheckkList.push(checkedItem);
+    localStorage.setItem("checkList", JSON.stringify(newCheckkList));
+  } else {
+    const storeCheckList = JSON.parse(localStorage.getItem("checkList"));
+    const idx = storeCheckList.findIndex((item) => item.id === +itemId);
+    storeCheckList.splice(idx, 1);
+
+    localStorage.setItem("checkList", JSON.stringify(storeCheckList));
+  }
+}
+
+checkAllBtn.addEventListener("change", (e) => handleAllChecked(e));
+
+checkboxs.forEach((checkbox, idx) => {
+  if (idx === 0) return;
+
+  const box = checkbox.children[0].querySelector("input");
+  const id = checkbox.getAttribute("id");
+
+  box.addEventListener("change", (e) => handleChecked(e, id));
+});
