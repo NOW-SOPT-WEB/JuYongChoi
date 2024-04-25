e26e0bc0baa1c9ed1a9d29ed5e40571f79685586
wuzoo <wndyd9911@gmail.com>
2024-04-26 00:18:27 +0900

    refactor: context, context provider 제거

--- week3/src/App.jsx
+++ week3/src/App.jsx
@@ -2,17 +2,14 @@ import { Global, ThemeProvider } from "@emotion/react";
 import { GlobalStyle } from "./styles/GlobalStyle";
 import Main from "./pages/Home";
 import { Theme } from "./styles/theme/theme";
-import { LevelInfoProvider } from "./context/Level";
 
 function App() {
   return (
     <>
-      <LevelInfoProvider>
-        <ThemeProvider theme={Theme}>
-          <Global styles={GlobalStyle} />
-          <Main />
-        </ThemeProvider>
-      </LevelInfoProvider>
+      <ThemeProvider theme={Theme}>
+        <Global styles={GlobalStyle} />
+        <Main />
+      </ThemeProvider>
     </>
   );
 }
