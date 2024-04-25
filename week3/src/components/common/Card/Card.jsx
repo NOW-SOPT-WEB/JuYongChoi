1c85c46dd06e84db96a3f40c32cc81d84eed64c0
wuzoo <wndyd9911@gmail.com>
2024-04-26 00:14:33 +0900

    refactor: style 수정 + 카드 애니메이션을 위한 JSX 수정

--- week3/src/components/common/Card/Card.jsx
+++ week3/src/components/common/Card/Card.jsx
@@ -1,14 +1,18 @@
 import backImg from "../../../assets/card_back.png";
-import { containerStyle, backImgStyle, frontImgStyle } from "./Card.style";
+import {
+  cardStyle,
+  backImgStyle,
+  frontImgStyle,
+  wrapperStyle,
+} from "./Card.style";
 
 const Card = ({ isClicked, onFlip, frontImg }) => {
   return (
-    <div onClick={onFlip} css={containerStyle}>
-      {isClicked ? (
+    <div css={wrapperStyle} onClick={onFlip}>
+      <div css={cardStyle(isClicked)}>
         <img src={frontImg} css={frontImgStyle} />
-      ) : (
         <img src={backImg} css={backImgStyle} />
-      )}
+      </div>
     </div>
   );
 };
