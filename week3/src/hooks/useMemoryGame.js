304eb11ab677be68b6544ce9ccd2444ac612d150
wuzoo <wndyd9911@gmail.com>
2024-04-26 17:01:37 +0900

    refactor: 불필요한 변수 제거 -> useEffect 지양

--- week3/src/hooks/useMemoryGame.js
+++ week3/src/hooks/useMemoryGame.js
@@ -1,29 +1,14 @@
 import { useState } from "react";
 
 export const useMemoryGame = () => {
-  const [successCount, setSuccessCount] = useState(0);
   const [isRestarted, setIsRestarted] = useState(false);
 
-  const handleCorrect = (count) => {
-    if (count <= successCount) return;
-
-    setSuccessCount(count);
-  };
-
-  const handleReset = () => {
-    setSuccessCount(0);
-  };
-
-  const handleRestart = () => {
-    handleReset();
+  const handleRestarted = () => {
     setIsRestarted((prev) => !prev);
   };
 
   return {
-    successCount,
     isRestarted,
-    handleCorrect,
-    handleReset,
-    handleRestart,
+    handleRestarted,
   };
 };
