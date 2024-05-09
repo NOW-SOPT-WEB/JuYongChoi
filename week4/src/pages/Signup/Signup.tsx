652e185c131c2c7316d1ea3ba2cbc429a697ef36
wuzoo <wndyd9911@gmail.com>
2024-05-09 16:57:10 +0900

    feat: 비밀번호 유효성 체크 + 전화번호 formating 하여 submit, SignupForm 컴포넌트 활용

--- week4/src/pages/Signup/Signup.tsx
+++ week4/src/pages/Signup/Signup.tsx
@@ -1,27 +1,40 @@
 import { postSignUp } from "@/api";
 import Box from "@components/common/Box/Box";
-import Button from "@components/common/Button/Button";
 import Flex from "@components/common/Flex/Flex";
 import Heading from "@components/common/Heading/Heading";
-import Input from "@components/common/Input/Input";
 import { Theme } from "@styles/theme";
 import axios from "axios";
-import { SubmitHandler, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
-import { formStyle } from "./Signup.style";
 import { UserDataType } from "@/types/api";
 import { HTTP_STATUS_CODE } from "@/constants/api";
 import { URL_MAP } from "@/constants/url";
 import { GUIDE_MESSAGE } from "@/constants/message";
+import SignUpForm from "@/components/Signup/SignupForm";
+import { checkPasswordValidation } from "@/utils";
 
 const SignUpPage = () => {
   const navigate = useNavigate();
 
-  const { register, handleSubmit } = useForm<UserDataType>();
+  const { register, handleSubmit, watch, setFocus } = useForm<UserDataType>();
 
-  const onSubmit: SubmitHandler<UserDataType> = async (data) => {
+  const onSubmit = handleSubmit(async (data) => {
+    if (
+      !data.authenticationId ||
+      !data.nickname ||
+      !data.password ||
+      !data.phone ||
+      !checkPasswordValidation(data.password) // 비밀번호 유효성 체크
+    )
+      return;
+
     try {
-      await postSignUp(data);
+      const formData = {
+        ...data,
+        phone: data.phone.replace(/(\d{3})(\d{4})(\d{4})/, "$1-$2-$3"), // '-' 포함시키기
+      };
+
+      await postSignUp(formData);
       alert(GUIDE_MESSAGE.COMPLETE_SIGNUP);
       navigate(URL_MAP.LOGIN);
     } catch (error) {
@@ -36,7 +49,7 @@ const SignUpPage = () => {
         }
       }
     }
-  };
+  });
 
   const 뒤로가기 = () => {
     navigate(-1);
@@ -59,31 +72,13 @@ const SignUpPage = () => {
         }}
       >
         <Heading size="xxLarge">회원가입 페이지</Heading>
-
-        <form css={formStyle} onSubmit={handleSubmit(onSubmit)}>
-          <Input {...register("authenticationId")} size="large" label="ID" />
-          <Input
-            supportingText={GUIDE_MESSAGE.PASSWORD}
-            {...register("password")}
-            size="large"
-            label="비밀번호"
-          />
-          <Input {...register("nickname")} size="large" label="닉네임" />
-          <Input
-            supportingText={GUIDE_MESSAGE.PHONENUM}
-            {...register("phone")}
-            size="large"
-            label="전화번호"
-          />
-          <Flex styles={{ justify: "center", gap: "16px", marginTop: "10px" }}>
-            <Button type="submit" variant="primary">
-              회원가입
-            </Button>
-            <Button variant="secondary" onClick={뒤로가기}>
-              뒤로가기
-            </Button>
-          </Flex>
-        </form>
+        <SignUpForm
+          register={register}
+          onSubmit={onSubmit}
+          onWatch={watch}
+          onGoBack={뒤로가기}
+          onFocus={setFocus}
+        />
       </Flex>
     </Box>
   );
