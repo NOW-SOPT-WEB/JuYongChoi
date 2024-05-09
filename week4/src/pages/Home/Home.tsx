10b16188b9c049be0491bef9bea277f215bdb83f
wuzoo <wndyd9911@gmail.com>
2024-05-09 16:54:35 +0900

    feat: Img -> Video 컴포넌트로 대체 + space로 재생/멈춤 기능 구현

--- week4/src/pages/Home/Home.tsx
+++ week4/src/pages/Home/Home.tsx
@@ -1,15 +1,37 @@
 import Box from "@components/common/Box/Box";
 import Button from "@components/common/Button/Button";
 import Flex from "@components/common/Flex/Flex";
-import Img from "@components/common/Img/Img";
 import { Theme } from "@/styles/theme";
 import { useNavigate, useParams } from "react-router-dom";
-import img from "@assets/img.jpeg";
 import { URL_MAP } from "@/constants/url";
+import Video from "@/components/common/Video/Video";
+import { useEffect, useRef, useState } from "react";
+import mainVideo from "@assets/Example.mp4";
 
 const HomePage = () => {
   const navigate = useNavigate();
   const { authenticationId } = useParams();
+
+  const [isVideoHover, setIsVideoHover] = useState(false);
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  /** Space로 재생, 멈춤 제어하는 함수 */
+  const handleKeyDown = (e: KeyboardEvent) => {
+    const { code } = e;
+
+    if (!videoRef.current) return;
+    if (code === "Space") {
+      videoRef.current?.paused
+        ? videoRef.current.play()
+        : videoRef.current.pause();
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("keypress", handleKeyDown);
+
+    return () => window.removeEventListener("keypress", handleKeyDown);
+  });
 
   const 마이페이지가기 = () => {
     navigate(`${URL_MAP.MYPAGE}/${authenticationId}`);
@@ -29,7 +51,16 @@ const HomePage = () => {
       }}
     >
       <Flex styles={{ direction: "column", align: "center", gap: "12px" }}>
-        <Img src={img} shape="circle" width="300px" height="300px" />
+        <Video
+          ref={videoRef}
+          onMouseOver={() => setIsVideoHover(true)}
+          onMouseLeave={() => setIsVideoHover(false)}
+          controls={isVideoHover}
+          width={"400px"}
+          height={"300px"}
+          autoplay
+          src={mainVideo}
+        />
         <Flex styles={{ gap: "16px", marginTop: "12px" }}>
           <Button variant="primary" onClick={마이페이지가기}>
             내 정보
